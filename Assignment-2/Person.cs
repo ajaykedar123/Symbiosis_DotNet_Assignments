3d86b33474f401f5f5c7b522991a3e32d4fdc446
ajaykedar123 <155829407+ajaykedar123@users.noreply.github.com>
2024-07-29 18:28:42 +0530

    Add files via upload

---
+++ Assignment-2/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class Person: IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //Default Constructor
+        public Person() { }
+
+        //Parameterized Constructor
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name} \t Age: {Age}");
+        }
+
+        //CompareTo method from IComparable<> Interface
+        public int CompareTo(Person person)
+        {
+            if (this.Age > person.Age)
+            {
+                return 1;
+            }
+            else if (this.Age < person.Age)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //List object
+            List<Person> list = new List<Person>();
+            var p1 = new Person("Ajay Kedar", 22);
+            var p2 = new Person("Ravindra Khadangale", 20);
+            var p3 = new Person("Shubham  Suryawanshi", 23);
+
+            //Adding in the list
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+
+            Console.WriteLine("\n----- Sorting According to Age -----");
+            Console.WriteLine("\n--------- Before Sorting ---------");
+            foreach (Person p in list)
+            {
+                p.PrintDetails();
+            }
+
+            list.Sort();
+
+            Console.WriteLine("\n--------- After Sorting ---------");
+            foreach (Person p in list)
+            {
+                p.PrintDetails();
+            }
+
+            Console.ReadLine(); 
+        }
+
+    }
+}
---
+++ Assignment-3/Point3D.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    internal class Point3D :IFormattable
+    {
+
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        // Parameterized Constructor for initializing the members
+        public Point3D(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // Override ToString() method
+        public override string ToString()
+        {
+            return $"Point3D(x: {x}, y: {y}, z: {z})";
+        }
+
+        // Implement IFormattable Interface
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format)) format = "G";
+            switch (format)
+            {
+                case "G":
+                case "C":
+                    return $"({x}, {y}, {z})";
+                case "X":
+                    return x.ToString();
+                case "Y":
+                    return y.ToString();
+                case "Z":
+                    return z.ToString();
+                default:
+                    throw new FormatException($"The {format} format string is not supported.");
+            }
+        }
+
+        // Implement the IFormattable.ToString with default format provider
+        public string ToString(string format)
+        {
+            return ToString(format, null);
+        }
+        static void Main(string[] args)
+        {
+            // Create an instance of Point3D
+            Point3D point = new Point3D(9.2f, 7.3f, 1.1f);
+
+            // Print x, y, and z individually and combined using various formats
+
+            // Print x
+            Console.WriteLine(point.ToString("X"));
+            // Print y
+            Console.WriteLine(point.ToString("Y"));
+            // Print z
+            Console.WriteLine(point.ToString("Z"));
+            // Print combined (x, y, z)
+            Console.WriteLine(point.ToString("C"));
+            // Print combined (x, y, z)
+            Console.WriteLine(point.ToString("G")); 
+
+
+            Console.WriteLine($"X: {point.ToString("X")}");
+            Console.WriteLine($"Y: {point.ToString("Y")}");
+            Console.WriteLine($"Z: {point.ToString("Z")}");
+            Console.WriteLine($"Combined (C): {point.ToString("C")}");
+            Console.WriteLine($"General (G): {point.ToString("G")}");
+
+            Console.ReadKey();
+        }
+    }
+}
